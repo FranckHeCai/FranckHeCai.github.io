b1a9fb0867e1e40e763df62a0545667dd29f9c37
Franck Cai <senlouboi.egame@gmail.com>
2025-04-04 16:06:40 +0200

    astro config and run scripts

--- webportfolio/astro.config.mjs
+++ webportfolio/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://franckhecai.github.io/",
-  base: "webportfolio",
+  base: "/webportfolio/",
 });
